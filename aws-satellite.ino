66003ed7dfa120b69d0dae5dc1a8329ad76689c8
Henry Hunt <henryhunt.dev@gmail.com>
2020-07-23 14:38:28 +0100

    Various wide-ranging changes and improvements

--- aws-satellite.ino
+++ aws-satellite.ino
@@ -1,24 +1,22 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
-int satelliteIdPin1 = 4;
-int satelliteIdPin2 = 5;
+#define ID_PIN 4
+
+bool configured = false;
 
 bool windSpeedEnabled = false;
-int windSpeedPin = 0;
+int windSpeedPin;
 bool windDirectionEnabled = false;
-int windDirectionPin = 0;
+int windDirectionPin;
 
-int windSpeedSamplingBucket = 1;
-int windSpeedSamplingBucket1 = 0;
-int windSpeedSamplingBucket2 = 0;
+int windSpeedCount = 0;
 
 
 void setup()
 {
+    pinMode(ID_PIN, INPUT_PULLUP);
     Serial.begin(9600);
-    pinMode(satelliteIdPin1, INPUT_PULLUP);
-    pinMode(satelliteIdPin2, INPUT_PULLUP);
 
     while (true)
     {
@@ -32,98 +30,114 @@ void setup()
             if (Serial.available())
             {
                 char newChar = Serial.read();
+
                 if (newChar != '\n')
                     command[position++] = newChar;
                 else commandEnded = true;
             }
         }
 
-        // Respond with the ID of the device (encoded as a binary number using two digital pins)
-        if (strncmp(command, "PING", 4) == 0)
-        {
-            int satelliteId =
-                (2 * !digitalRead(satelliteIdPin1)) + !digitalRead(satelliteIdPin2); // Binary to denary
 
-            char response[7] = { '\0' };
-            sprintf(response, "PING %d", satelliteId);
+        // PING command. Respond with a description of the device
+        if (strncmp(command, "PING", 4) == 0)
+            Serial.println("AWS Satellite Device\n");
+
+        // ID command. Respond with the ID of the satellite device
+        if (strncmp(command, "ID", 2) == 0)
+        {
+            char response[3] = { '\0' };
+
+            // ID is set in hardware by toggling a digital pin (which provides two unique IDs)
+            sprintf(response, "%d\n", !digitalRead(ID_PIN) + 1);
             Serial.println(response);
         }
 
-        // Configure the device with a JSON string indicating sensor information
+        // CONFIG command. Configure the satellite device using the sent JSON
         else if (strncmp(command, "CONFIG {", 8) == 0)
         {
-            StaticJsonDocument<JSON_OBJECT_SIZE(4)> document;
-            DeserializationError jsonStatus = deserializeJson(document, command + 7);
-
-            if (jsonStatus != DeserializationError::Ok)
+            if (configure(command + 7))
             {
-                Serial.println("CONFIG ERROR");
+                configured = true;
+                Serial.println("CONFIG\n");
+            }
+            else
+            {
+                Serial.println("CONFIG ERROR\n");
                 continue;
             }
-
-            bool fieldError = false;
-            JsonObject jsonObject = document.as<JsonObject>();
-
-            if (jsonObject.containsKey("windSpeedEnabled"))
-            {
-                JsonVariant value = jsonObject.getMember("windSpeedEnabled");
-                if (value.is<bool>())
-                {
-                    windSpeedEnabled = value;
-                    if (windSpeedEnabled)
-                    {
-                        if (jsonObject.containsKey("windSpeedPin"))
-                        {
-                            JsonVariant value2 = jsonObject.getMember("windSpeedPin");
-                            if (value2.is<int>())
-                                windSpeedPin = value2;
-                            else fieldError = true;
-                        } else fieldError = true;
-                    }
-                } else fieldError = true;
-            } else fieldError = true;
-
-            if (jsonObject.containsKey("windDirectionEnabled"))
-            {
-                JsonVariant value = jsonObject.getMember("windDirectionEnabled");
-                if (value.is<bool>())
-                {
-                    windDirectionEnabled = value;
-                    if (windDirectionEnabled)
-                    {
-                        if (jsonObject.containsKey("windDirectionPin"))
-                        {
-                            JsonVariant value2 = jsonObject.getMember("windDirectionPin");
-                            if (value2.is<int>())
-                                windDirectionPin = value2;
-                            else fieldError = true;
-                        } else fieldError = true;
-                    }
-                } else fieldError = true;
-            } else fieldError = true;
-
-            if (fieldError)
-            {
-                Serial.println("CONFIG ERROR");
-                continue;
-            }
-            else Serial.println("CONFIG");
 
             if (windSpeedEnabled)
-            {
                 pinMode(windSpeedPin, INPUT_PULLUP);
-                attachInterrupt(digitalPinToInterrupt(windSpeedPin), windSpeedInterrupt, FALLING);
-            }
         }
 
-        // Exit setup and move to the main loop function
+        // START command. Exit setup() and move to loop() for sensor sampling
         if (strncmp(command, "START", 5) == 0)
         {
-            Serial.println("START");
-            break;
+            if (configured)
+            {
+                Serial.println("START\n");
+                attachInterrupt(digitalPinToInterrupt(windSpeedPin), windSpeedInterrupt, FALLING);
+                break;
+            }
+            else Serial.println("START ERROR\n");
         }
     }
 }
+
+bool configure(char* json)
+{
+    StaticJsonDocument<JSON_OBJECT_SIZE(4)> document;
+    DeserializationError jsonStatus = deserializeJson(document, json);
+
+    if (jsonStatus != DeserializationError::Ok)
+        return;
+
+    bool fieldError = false;
+    JsonObject jsonObject = document.as<JsonObject>();
+
+    if (jsonObject.containsKey("windSpeedEnabled"))
+    {
+        JsonVariant value = jsonObject.getMember("windSpeedEnabled");
+        if (value.is<bool>())
+        {
+            windSpeedEnabled = value;
+            if (windSpeedEnabled)
+            {
+                if (jsonObject.containsKey("windSpeedPin"))
+                {
+                    value = jsonObject.getMember("windSpeedPin");
+
+                    if (value.is<int>())
+                        windSpeedPin = value;
+                    else return false;
+                } else return false;
+            }
+        } else return false;
+    } else return false;
+
+    if (jsonObject.containsKey("windDirectionEnabled"))
+    {
+        JsonVariant value = jsonObject.getMember("windDirectionEnabled");
+        if (value.is<bool>())
+        {
+            windDirectionEnabled = value;
+            if (windDirectionEnabled)
+            {
+                if (jsonObject.containsKey("windDirectionPin"))
+                {
+                    value = jsonObject.getMember("windDirectionPin");
+
+                    if (value.is<int>())
+                        windDirectionPin = value;
+                    else return false;
+                } else return false;
+            }
+        } else return false;
+    } else return false;
+
+    return true;
+}
+
 
 void loop()
 {
@@ -139,56 +153,63 @@ void loop()
             if (Serial.available())
             {
                 char newChar = Serial.read();
+
                 if (newChar != '\n')
                     command[position++] = newChar;
                 else commandEnded = true;
             }
         }
 
-        // Read sensor values and respond with a JSON string
+
+        // SAMPLE command. Read sensor values and return them in a JSON string
         if (strncmp(command, "SAMPLE", 6) == 0)
         {
-            if (windSpeedSamplingBucket == 1)
-                windSpeedSamplingBucket = 2;
-            else windSpeedSamplingBucket = 1;
+            int windSpeed;
+            if (windSpeedEnabled)
+            {
+                windSpeed = windSpeedCount;
+                windSpeedCount = 0;
+            }
 
-            int windDirection = 0;
+            int windDirection;
+
+            // Read wind direction
             if (windDirectionEnabled)
             {
-                float windDirectionADC = analogRead(windDirectionPin);
+                float adcValue = analogRead(windDirectionPin);
                 
-                // Convert voltage to degrees
-                if (windDirectionADC < 0.25) windDirectionADC = 0.25;
-                else if (windDirectionADC > 4.75) windDirectionADC = 4.75;
+                if (adcValue < 0.25)
+                    adcValue = 0.25;
+                else if (adcValue > 4.75)
+                    adcValue = 4.75;
                 
-                windDirection = round(map(windDirectionADC, 0.25, 4.75, 0, 360));
-                if (windDirection == 360) windDirection = 0;
+                windDirection = round(map(adcValue, 0.25, 4.75, 0, 360));
+
+                if (windDirection == 360)
+                    windDirection = 0;
             }
 
-            const char* format = "SAMPLE {\"windSpeed\":%d,\"windDirection\":%d}";
-            char samples[100] = { '\0' };
 
-            int windSpeed = 0;
-            if (windSpeedSamplingBucket == 2)
-            {
-                windSpeed = windSpeedSamplingBucket1;
-                windSpeedSamplingBucket1 = 0;
-            }
-            else
-            {
-                windSpeed = windSpeedSamplingBucket2;
-                windSpeedSamplingBucket2 = 0;
-            }
+            char sample_out[50] = { '\0' };
+            strcat(sample_out, "SAMPLE {");
+            int length = 8;
 
-            sprintf(samples, format, windSpeed, windDirection);
-            Serial.println(samples);
+            if (windSpeedEnabled)
+                length += sprintf(sample_out + length, "\"windSpeed\":%d", windSpeed);
+            else length += sprintf(sample_out + length, "\"windSpeed\":null");
+
+            if (windDirectionEnabled)
+                length += sprintf(sample_out + length, ",\"windDirection\":%d", windDirection);
+            else length += sprintf(sample_out + length, ",\"windDirection\":null");
+
+            strcat(sample_out + length, "}");
+            
+            Serial.println(sample_out);
         }
     }
 }
 
 void windSpeedInterrupt()
 {
-    if (windSpeedSamplingBucket == 1)
-        windSpeedSamplingBucket1++;
-    else windSpeedSamplingBucket2++;
+    windSpeedCount++;
 }
